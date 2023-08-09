9e65f186cdda4e6b6eb6283c99d2a86e01ba5019
kausar.akther@gmail.com <kausar.akther@gmail.com>
2023-08-09 17:05:47 -0400

    Add second header

--- src/components/Header.js
+++ src/components/Header.js
@@ -84,7 +84,60 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-        </header>  
+        </header> 
+        <div className="header-bottom py-3">
+          <div className="container-xxl">
+            <div className="row">
+              <div className="col-12">
+                <div className="menu-bottom d-flex laign-items-center gap-30">
+                  <div>
+                    <div className="dropdown">
+                      <button 
+                        className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" 
+                        type="button" 
+                        id="dropdownMenuButton1" 
+                        data-bs-toggle="dropdown" 
+                        aria-expanded="false"
+                      >
+                        <img src="./images/menu.svg" alt="menu"/>
+                        <span className='me-5 d-inline-block'>Shop Categories</span>
+                      </button>
+                      <ul 
+                        className="dropdown-menu" 
+                        aria-labelledby="dropdownMenuButton1"
+                      >
+                        <li>
+                          <Link className="dropdown-item text-white" to="">
+                            Action
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item text-white" to="">
+                            Another action
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item text-white" to="">
+                            Something else here
+                          </Link>
+                        </li>
+                    </ul>
+                  </div>
+                  </div>
+                  <div className="menu-links">
+                    <div className="d-flex laign-items-center gap-15">
+                      <NavLink to="/">Home</NavLink>
+                      <NavLink to="/">Our Store</NavLink>
+                      <NavLink to="/">Blogs</NavLink>
+                      <NavLink to="/contact">Contact</NavLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
     </>
   )
 }
