b718cb80ba4aeaa494505ef903b2d766463ed071
kausar.akther@gmail.com <kausar.akther@gmail.com>
2023-08-09 11:28:11 -0400

    Added header and images

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,45 +48,51 @@ const Header = () => {
                     <div className="col-5">
                       <div className="header-upper-links d-flex align-items-center justify-content-between">
                         <div>
-                          <Link>
-                            <img src="./images/compare.svg" alt=""/>
-                            <p>
+                          <Link className="d-flex align-items-center gap-10 text-white">
+                            <img src="./images/compare.svg" alt="compare"/>
+                            <p className='mb-0'>
+                              Compare <br /> Product
                             </p>
                           </Link>
                         </div>
                         <div>
-                          <Link>
-                            <img src="" alt=""/>
-                            <p>
+                          <Link className="d-flex align-items-center gap-10 text-white">
+                            <img src="./images/wishlist.svg" alt="wishlist"/>
+                            <p className='mb-0'>
+                              Favourite <br /> Wishlist
                             </p>
                           </Link>
                         </div>
                         <div>
-                          <Link>
-                            <img src="" alt=""/>
-                            <p>
+                          <Link className="d-flex align-items-center gap-10 text-white">
+                            <img src="./images/user.svg" alt="user"/>
+                            <p className='mb-0'>
+                              Log in <br /> My Account
                             </p>
                           </Link>
                         </div>
                         <div>
-                          <Link>
-                            <img src="" alt=""/>
-                            <p>
-                            </p>
+                          <Link className="d-flex align-items-center gap-10 text-white">
+                            <img src="./images/cart.svg" alt="cart"/>
+                            <div className="d-flex flex-column gap-10">
+                              <span className="badge bg-white text-dark">0</span>
+                              <p className='mb-0'>$ 500</p>
+                            </div>
                           </Link>
                         </div>
                       </div>
                     </div>
                 </div>
             </div>
-        </header>
-      <Link>
-        <img src="./images/acc.jpg" alt="" />
-        <img src="./images/blog-1.jpg" alt="" />
-        <img src="./images/brand-02.png" alt="" />
-      </Link> 
+        </header>  
     </>
   )
 }
 
 export default Header
+
+// <Link>
+//         <img src="./images/acc.jpg" alt="" />
+//         <img src="./images/blog-1.jpg" alt="" />
+//         <img src="./images/brand-02.png" alt="" />
+//       </Link>
