ec5a0b1f0666fe77a08d0ed6f316739f4fc5f6d7
kausar.akther@gmail.com <kausar.akther@gmail.com>
2023-08-16 14:15:41 -0400

    Added Home wrapper 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,11 @@ import { NavLink, Link } from 'react-router-dom';
 const Home = () => {
   return (
     <>
-      <section className="home-wrapper-1 py-5">
+    <section className="home-wrapper-1 py-5">
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <div className="main-banner position-relative p-3">
+              <div className="main-banner position-relative">
               <img 
                 src="./images/main-banner-1.jpg" 
                 className="img-fluid rounded-3" 
@@ -22,10 +22,62 @@ const Home = () => {
               </div>       
               </div>
             </div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative ">
+                  <img 
+                    src="./images/catbanner-01.jpg" 
+                    className="img-fluid rounded-3" 
+                    alt="catbanner-01"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>BEST SALE</h4>
+                    <h5>Laptops Max</h5>
+                    <p>From $1699.00 or <br/> $64.62/mo.</p>
+                  </div>       
+                </div>
+                <div className="small-banner position-relative ">
+                  <img 
+                    src="./images/catbanner-02.jpg" 
+                    className="img-fluid rounded-3" 
+                    alt="catbanner-02"
+                  />
+                <div className="small-banner-content position-absolute">
+                  <h4>15% OFF</h4>
+                  <h5>Smartwatch 7</h5>
+                  <p>Shop the latest brand <br/> styles and colors</p>
+                </div>       
+                </div>
+                <div className="small-banner position-relative ">
+                  <img 
+                    src="./images/catbanner-03.jpg" 
+                    className="img-fluid rounded-3" 
+                    alt="catbanner-02"
+                  />
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>Buy IPad Air</h5>
+                  <p>From $599.00 or <br/> $49.91/mo.</p>
+                </div>       
+              </div>
+                <div className="small-banner position-relative ">
+                  <img 
+                    src="./images/catbanner-04.jpg" 
+                    className="img-fluid rounded-3" 
+                    alt="catbanner-02"
+                  />
+                <div className="small-banner-content position-absolute">
+                  <h4>FREE ARRIVAL</h4>
+                  <h5>AirPods Max</h5>
+                  <p>High-fidelity playback & <br/> $41.62/mo.</p>
+                </div>       
+                </div>
+              </div>
+            </div>
           </div>
         </div>
      </section>
+     
     </>
   )
 }
