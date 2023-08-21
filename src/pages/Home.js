26adf7a4c8618ea8512b32acd9b5a8e990c36b5d
kausar.akther@gmail.com <kausar.akther@gmail.com>
2023-08-21 16:27:54 -0400

    Add marquee inner wrapper

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {  Link } from 'react-router-dom';
+import Marquee from "react-fast-marquee";
+
 
 const Home = () => {
   return (
@@ -188,6 +190,39 @@ const Home = () => {
         </div>
       </div>
      </section>
+     <section className="home-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="marquee-inner-wrapper bg-white card-wrapper">
+                <Marquee className="d-flex">
+                  <div className="mx-4 w-25">
+                    <img src= "./images/brand-01.png" alt="brand" />  
+                  </div> 
+                  <div className="mx-4 w-25">
+                    <img src= "./images/brand-02.png" alt="brand" />  
+                  </div> 
+                  <div className="mx-4 w-25">
+                    <img src= "./images/brand-03.png" alt="brand" />  
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src= "./images/brand-04.png" alt="brand" />  
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src= "./images/brand-05.png" alt="brand" />  
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src= "./images/brand-06.png" alt="brand" />  
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src= "./images/brand-07.png" alt="brand" />  
+                  </div>
+                </Marquee>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
