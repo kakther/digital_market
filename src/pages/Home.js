7a0a576e80506492624228f4d00eb98b27391a72
kausar.akther@gmail.com <kausar.akther@gmail.com>
2023-08-22 13:28:56 -0400

    Builded blog cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {  Link } from 'react-router-dom';
 import Marquee from "react-fast-marquee";
+import BlogCard from '../components/BlogCard';
 
 
 const Home = () => {
@@ -190,7 +191,7 @@ const Home = () => {
         </div>
       </div>
      </section>
-     <section className="home-wrapper py-5">
+     <section className="home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
@@ -222,7 +223,24 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </section>
+     </section>
+     <section className="marque-wrapper">
+     </section>
+     <section className="blog-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="section-heading">
+              Our Latest Blogs
+            </h3>
+          </div>
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+        </div>
+      </div>
+     </section>
     </>
   )
 }
